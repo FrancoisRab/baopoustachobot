e3963b024ccd791a595df2311f1e6d9b305afe78
Francois rabanel <rabanel.francois@gmail.com>
2018-06-07 12:53:33 +0200

    testing deploy procfile bug

--- baopoustachobot.py
+++ baopoustachobot.py
@@ -1,8 +1,11 @@
 import random
 import requests
+import asyncio
+import os
 from discord.ext.commands import Bot
 from discord import Game
-# import os
+
+
 
 # TOKEN = 'TOKEN'   #TOKEN = os.environ['TOKEN']
 # CR_TOKEN = 'CR_TOKEN'   #CR_TOKEN = os.environ['CR_TOKEN']
@@ -95,5 +98,14 @@ async def clan():
                    "- Score: " + str(data["score"]) + "\n" +
                    "- Membres: " + str(data["memberCount"]) + "/50 \n" +
                    "- Dons: " + str(data["donations"]))
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
 
+
+client.loop.create_task(list_servers())
 client.run(TOKEN)
